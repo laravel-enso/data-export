682c69a8443c1844befa3e0e87b5797dd714c33c
mishu <mishu@xtelecom.ro>
2019-02-03 17:03:52 +0200

    removes own enum in favor of IOStatuses; implements the new io contract / uses the new trait

--- src/app/Models/DataExport.php
+++ src/app/Models/DataExport.php
@@ -4,6 +4,7 @@ namespace LaravelEnso\DataExport\app\Models;
 
 use LaravelEnso\IO\app\Enums\IOTypes;
 use Illuminate\Database\Eloquent\Model;
+use LaravelEnso\IO\app\Traits\HasIOStatuses;
 use LaravelEnso\IO\app\Contracts\IOOperation;
 use LaravelEnso\TrackWho\app\Traits\CreatedBy;
 use LaravelEnso\FileManager\app\Traits\HasFile;
@@ -13,7 +14,7 @@ use LaravelEnso\Multitenancy\app\Traits\SystemConnection;
 
 class DataExport extends Model implements Attachable, VisibleFile, IOOperation
 {
-    use CreatedBy, HasFile, SystemConnection;
+    use CreatedBy, HasIOStatuses, HasFile, SystemConnection;
 
     protected $fillable = ['name', 'entries', 'status'];
 
@@ -27,23 +28,8 @@ class DataExport extends Model implements Attachable, VisibleFile, IOOperation
         return true;
     }
 
-    public function name()
-    {
-        return $this->name;
-    }
-
-    public function entries()
-    {
-        return $this->entries;
-    }
-
     public function type()
     {
         return IOTypes::Export;
-    }
-
-    public function status()
-    {
-        return $this->status;
     }
 }
