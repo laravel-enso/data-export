2f464f7bced789633f29741315cce181a8913758
Mihai <mihai.ocneanu@gmail.com>
2020-11-27 17:12:02 +0200

    refactor due to files updates

--- src/Services/ExcelExport.php
+++ src/Services/ExcelExport.php
@@ -158,7 +158,7 @@ class ExcelExport
         $this->writer->close();
 
         $this->dataExport->attach(
-            new File($this->filePath()),
+            $this->filePath(),
             $this->exporter->filename(),
             $this->user
         );
