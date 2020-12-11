8198e2712f69f7b95853594a1355b74468b795f4
mishu <mishu@xtelecom.ro>
2020-12-11 09:09:16 +0200

    fixes scope

--- src/Models/DataExport.php
+++ src/Models/DataExport.php
@@ -3,9 +3,9 @@
 namespace LaravelEnso\DataExport\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\Config;
 use LaravelEnso\DataExport\Contracts\ExportsExcel;
 use LaravelEnso\DataExport\Enums\Statuses;
