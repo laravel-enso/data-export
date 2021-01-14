e9503e732402f8fd747be85115ae16af3708d52e
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2021-01-14 22:42:14 +0200

    adds sync excel attaching ability

--- src/Models/DataExport.php
+++ src/Models/DataExport.php
@@ -6,11 +6,16 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Config;
-use LaravelEnso\DataExport\Contracts\ExportsExcel;
+use Illuminate\Support\Str;
+use LaravelEnso\DataExport\Contracts\ExportsExcel as AsyncExcel;
 use LaravelEnso\DataExport\Enums\Statuses;
 use LaravelEnso\DataExport\Exceptions\Exception;
-use LaravelEnso\DataExport\Services\ExcelExport;
+use LaravelEnso\DataExport\Notifications\ExportDone;
+use LaravelEnso\DataExport\Services\ExcelExport as AsyncExporter;
+use LaravelEnso\Excel\Contracts\ExportsExcel as SyncExcel;
+use LaravelEnso\Excel\Services\ExcelExport as SyncExporter;
 use LaravelEnso\Files\Contracts\Attachable;
 use LaravelEnso\Files\Contracts\AuthorizesFileAccess;
 use LaravelEnso\Files\Traits\FilePolicies;
@@ -19,7 +24,9 @@ use LaravelEnso\Helpers\Services\Decimals;
 use LaravelEnso\Helpers\Traits\CascadesMorphMap;
 use LaravelEnso\IO\Contracts\IOOperation;
 use LaravelEnso\IO\Enums\IOTypes;
+use LaravelEnso\Tables\Notifications\ExportStarted;
 use LaravelEnso\TrackWho\Traits\CreatedBy;
+use UnexpectedValueException;
 
 class DataExport extends Model implements Attachable, IOOperation, AuthorizesFileAccess
 {
@@ -96,14 +103,52 @@ class DataExport extends Model implements Attachable, IOOperation, AuthorizesFil
         return $this->created_at;
     }
 
-    public static function excel(ExportsExcel $exporter): self
+    public static function excel($exporter): self
+    {
+        if ($exporter instanceof AsyncExcel) {
+            return self::asyncExcel($exporter);
+        }
+        if ($exporter instanceof SyncExcel) {
+            return self::syncExcel($exporter);
+        }
+
+        throw new UnexpectedValueException(
+            __('The exporter class must be in instance of ExportsExcel interface')
+        );
+    }
+
+    private static function asyncExcel(AsyncExcel $exporter): self
     {
         $export = self::factory()->create([
             'name' => $exporter->filename(),
             'total' => $exporter->query()->count(),
         ]);
 
-        (new ExcelExport($export, $exporter))->handle();
+        (new AsyncExporter($export, $exporter))->handle();
+
+        return $export;
+    }
+
+    private static function syncExcel(SyncExcel $exporter): self
+    {
+        $export = self::factory()->create([
+            'name' => $exporter->filename(),
+            'status' => Statuses::Processing,
+            'total' => 0,
+        ]);
+
+        $export->createdBy->notify(new ExportStarted($export->name));
+
+        $path = Str::afterLast((new SyncExporter($exporter))->save(), 'app/');
+
+        $count = Collection::wrap($exporter->sheets())
+            ->reduce(fn ($total, $sheet) => $total += count($exporter->rows($sheet)), 0);
+
+        $export->updateProgress($count);
+        $export->file->created_by = $export->created_by;
+        $export->file->attach($path ?? 'temp', $exporter->filename());
+        $export->update(['status' => Statuses::Finalized]);
+        $export->createdBy->notify(new ExportDone($export));
 
         return $export;
     }
