f398b2a75135ed6145959dd567bea9b22f845047
Mihai <mihai.ocneanu@gmail.com>
2022-03-02 13:34:20 +0200

    updated migrations

--- database/migrations/2018_09_10_100050_create_structure_for_data_export.php
+++ database/migrations/2018_09_10_100050_create_structure_for_data_export.php
@@ -2,9 +2,9 @@
 
 use LaravelEnso\Migrator\Database\Migration;
 
-class CreateStructureForDataExport extends Migration
+return new class extends Migration
 {
     protected array $permissions = [
         ['name' => 'export.cancel', 'description' => 'Cancel running export', 'is_default' => true],
     ];
-}
+};
